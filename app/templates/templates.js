8badbf57a93fd243531a817a77244aec3140c8cc
Mohamed Salem <mo.zaher.salem@gmail.com>
2017-05-08 20:42:40 +0400

    Create git repo

---
+++ app/templates/templates.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('templateStore.templates', ['ngRoute', 'ngCart'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/templates', {
+    templateUrl: 'templates/templates.html',
+    controller: 'TemplatesCtrl'
+  }).
+  when('/templates/:templateId', {
+    templateUrl: 'templates/template-details.html',
+    controller: 'TemplateDetailsCtrl'
+  });
+}]).
+
+controller('TemplatesCtrl', ['$scope','$http', 'ngCart', function($scope, $http, ngCart) {
+  $http.get('json/templates.json').success (function(data ) {
+    $scope.templates = data;
+  });
+}]).
+
+controller('TemplateDetailsCtrl', [
+    '$scope',
+    '$http',
+    '$routeParams',
+    '$filter',
+    'ngCart', 
+    function($scope, $http, $routeParams, $filter, ngCart ) {
+      var templateId = $routeParams.templateId;
+
+      $http.get('json/templates.json').success (function(data ) {
+        $scope.template = $filter('filter')(data, function(d){
+          return d.id == templateId;
+        })[0];
+        $scope.mainImage = $scope.template.images[0].name;
+      });
+
+      $scope.setImage = function(image) {
+        $scope.mainImage = image.name;
+      };
+
+      ngCart.setTaxRate(7.5);
+      ngCart.setShipping(2.99);
+
+    }
+
+]);
